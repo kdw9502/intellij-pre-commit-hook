8d8b180eb44e8bda0e493f01306a50e595dc7e76
GitHub Action <action@github.com>
2022-09-03 22:39:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/kdw9502/intellijprecommithook/MyBundle.kt
+++ src/main/kotlin/com/github/kdw9502/intellijprecommithook/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kdw9502.intellijprecommithook
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kdw9502/intellijprecommithook/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kdw9502/intellijprecommithook/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kdw9502.intellijprecommithook.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kdw9502.intellijprecommithook.MyBundle
 
 class MyApplicationService {
 
